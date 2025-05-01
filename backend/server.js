59221dee947268d15b9304d23f8f0aa45c6b748f
Shuiii <72098121+shuiiiiiii@users.noreply.github.com>
2025-05-01 18:40:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,23 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const port = 9000;
-const uri = "mongodb+srv://shuiii:leti@123@testing.fno66zl.mongodb.net/?retryWrites=true&w=majority&appName=testing";
+const uri = "mongodb+srv://shuiii:leti%40123@testing.fno66zl.mongodb.net/?retryWrites=true&w=majority&appName=testing";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 async function run() {
   try {
-    await mongoose.connect(uri);
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    // Ensures that the client will close when you finish/error
     await mongoose.disconnect();
   }
 }
-
 run().catch(console.dir);
+
 
 const doctor = mongoose.model('Doctor', mongoose.Schema({
     name: String,
