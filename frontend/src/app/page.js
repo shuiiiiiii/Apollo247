3e6053dfcc03c977e30d690fc1574148a9ce87e8
Shuiii <72098121+shuiiiiiii@users.noreply.github.com>
2025-05-02 21:55:36 +0530

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,6 @@
 'use client'
 
 import Image from "next/image";
-import { Truculenta } from "next/font/google";
 
 const addDoc = () => {
   const docName = document.querySelector("#docName").value;
